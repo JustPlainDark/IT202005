94350968bd1410268bff558b150ae5874f455f44
David Garcia <dg533@njit.edu>
2021-11-07 21:42:38 -0500

    Flash messages

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -28,24 +28,24 @@ require(__DIR__ . "/../../partials/nav.php");?>
    //TODO 3
    $hasError = false;
    if(empty($email)){
-       echo "Email must not be empty";
+       flash("Email must not be empty");
        $hasError = true;
    }
    $email = sanitize_email($email);
    if(!is_valid_email($email)){
-       echo "Invalid email address";
+       flash("Invalid email address");
        $hasError = true;
    }
    if(empty($password)){
-       echo "Password must not be empty";
+       flash("Password must not be empty");
        $hasError = true;
    }
    if(strlen($password) < 8) {
-       echo "Password too short";
+       flash("Password too short");
        $hasError = true;
    }
    if(!$hasError){
-       echo "Welcome, $email";
+       flash("Welcome, $email");
        //TODO 4
        $db = getDB();
         $stmt = $db->prepare("SELECT email, password from Users where email = :email");
@@ -57,19 +57,22 @@ require(__DIR__ . "/../../partials/nav.php");?>
                     $hash = $user["password"];
                     unset($user["password"]);
                     if (password_verify($password, $hash)) {
-                        echo "Welcome $email";
+                        flash("Welcome $email");
                         $_SESSION["user"] = $user;
                         die(header("Location: home.php"));
                     } else {
-                        echo "Invalid password";
+                        flash("Invalid password");
                     }
                 } else {
-                    echo "Email not found";
+                    flash("Email not found");
                 }
             }
         } catch (Exception $e) {
-            echo "<pre>" . var_export($e, true) . "</pre>";
+            flash("<pre>" . var_export($e, true) . "</pre>");
         }
        }
     }
+?>
+<?php
+require(__DIR__ . "/../../partials/flash.php");
 ?>
