8aad8e5c57db724b21b21e3911e9a669c0578889
JustPlainDark <dg533@njit.edu>
2021-11-18 12:06:09 -0500

    Login Enhancement but I forgot to save some things

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -16,7 +16,7 @@ require(__DIR__ . "/../../partials/nav.php");
     function validate(form) {
         //TODO 1: implement JavaScript validation
         //ensure it returns false for an error and true for success
-
+        
         return true;
     }
 </script>
