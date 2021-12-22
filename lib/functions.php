b57c598be6efadfd2363393d7e913b6b96a14f4b
David Garcia <dg533@njit.edu>
2021-12-21 21:45:53 -0500

    Saving and displaying scores

--- lib/functions.php
+++ lib/functions.php
@@ -130,6 +130,15 @@ function get_url($dest)
     //handle relative path
     return $BASE_PATH . $dest;
 }
+function get_random_str($length)
+{
+    //https://stackoverflow.com/a/13733588
+    //$bytes = random_bytes($length / 2);
+    //return bin2hex($bytes);
+
+    //https://stackoverflow.com/a/40974772
+    return substr(str_shuffle(str_repeat('0123456789abcdefghijklmnopqrstvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 36)), 0, $length);
+}
 function save_score($score, $user_id, $showFlash = false)
 {
     if ($user_id < 1) {
